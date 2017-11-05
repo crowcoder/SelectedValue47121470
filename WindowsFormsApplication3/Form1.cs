d5b0b986163e5c51097a18713b780507f0c0f75a
Bob <crowcoder@gmail.com>
2017-11-05 07:54:50 -0500

    Add project files.

---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<ListOfStuff> los = new List<ListOfStuff>
+            {
+                new ListOfStuff { intProperty = 1, stringProperty = "one" },
+                new ListOfStuff { intProperty = 2, stringProperty = "two" },
+                new ListOfStuff { intProperty = 3, stringProperty = "three" }
+            };
+
+            comboBox1.DataSource = los;
+            comboBox1.ValueMember = "intProperty";
+            comboBox1.DisplayMember = "stringProperty";
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            comboBox1.SelectedValue = 1;
+        }
+    }
+}
